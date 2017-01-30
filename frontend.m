69a95e546d25d98eb86a35278dcf5658bf001547
sbind <simon.binder88@googlemail.com>
2017-01-30 14:52:12 +0100

    AeroModule updated

--- frontend.m
+++ frontend.m
@@ -1,8 +1,14 @@
-
+clear classes
 clear all
-addpath('stdlib\xml_toolbox')
-
-
+%% add required modules
+addpath(genpath('ext'))
 addpath(genpath('linStaticAeroModule'))
 
-aeroModel=AeroModel('Goland.xml');
+%% think about a way of putting states into an/another input file
+%                       U   aoa beta    Ma      rho
+state=class_aero_state(100, 10*pi/180, 0,      0.0,    1.225);
+%% Setup Aero Model
+
+myAeroModel=AeroModel('testWing.xml',state);
+myAeroModel=myAeroModel.calculateForces();
+myAeroModel.plotCp();
--- linStaticAeroModule/AeroModel.m
+++ linStaticAeroModule/AeroModel.m
@@ -2,23 +2,49 @@ classdef AeroModel
     %UNTITLED Summary of this class goes here
     %   Detailed explanation goes here
     
-    properties (SetAccess=Hidden)
+    properties (GetAccess=private)
+        %Geometry class
         geometry
+        %Vortex Lattice Kernel
+        VLM
+    end
+    properties 
+        %Body grid for coupling
+        grid
+        %Forces in Body Fixed Frame
+        F 
     end
     
     methods
-        function obj=AeroModel(xmlFile)
-            addpath('dAEDalusNXT/aerodynamics')
-            addpath('dAEDalusNXT/geometry')
+        function obj=AeroModel(xmlFile, aeroState) %constructor
+            %read geofile
             obj.geometry=class_aircraft(xmlFile,1);
+            %aero grid settings
+            obj.geometry.grid_settings.x_max_grid_size= obj.geometry.reference.c_ref/16;
+            obj.geometry.grid_settings.y_max_grid_size= obj.geometry.reference.b_ref/80;
+            %compute grid
+            obj.geometry=obj.geometry.compute_grid();
+            %initialize vlm kernel
+            obj.VLM=class_VLM_solver(obj.geometry.grid,obj.geometry.te_idx, obj.geometry.panels,aeroState, obj.geometry.reference);
+            % use nvec2 for normal vector correction
+            obj.VLM=obj.VLM.update_nvec2(obj.geometry);
+            %store grid for coupling algorithms
+            obj.grid=obj.VLM.grid;
         end
-        function obj=calculateMatrices()
-            
+        function obj=calculateForces(obj)
+            %this function calculates the forces 
+            obj.VLM=obj.VLM.f_solve_std;
+            obj.F=obj.VLM.F_body;
         end
-        function obj=calculateForces()
-            
+        function plotGrid(obj)
+            %plots the computed grid
+            obj.geometry.plot_grid();
         end
-        function obj=updateGrid(grid)
+        function plotCp(obj)
+            %plots the computed grid
+            obj.VLM.plot_cp();
+        end
+        function obj=updateGrid(obj)
             
         end
     end
