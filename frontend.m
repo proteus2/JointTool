d180041ac159e8ece95d79311955acc9bca2a679
Mario Natella <m.natella@tudelft.nl>
2017-02-06 15:49:00 +0100

    Plot functions for structural model added.

--- frontend.m
+++ frontend.m
@@ -1,5 +1,7 @@
 clear classes
 clear all
+close all
+clc
 
 %% add required modules
 addpath(genpath('ext'))
@@ -8,12 +10,12 @@ addpath(genpath('linStaticStrModule'))
 
 %% think about a way of putting states into an/another input file
 %                       U   aoa beta    Ma      rho
-state=class_aero_state(100, 10*pi/180, 0,      0.0,    1.225);
+% state = class_aero_state(100, 10*pi/180, 0,      0.0,    1.225);
 
 %% Setup Aero Model
-myAeroModel=AeroModel('testWing.xml',state);
-myAeroModel=myAeroModel.calculateForces();
-myAeroModel.plotCp();
+% myAeroModel=AeroModel('testWing.xml',state);
+% myAeroModel=myAeroModel.calculateForces();
+% myAeroModel.plotCp();
 
 %% Setup Strutural Model
 % Class init.
--- linStaticStrModule/structuralModel.m
+++ linStaticStrModule/structuralModel.m
@@ -21,11 +21,48 @@ classdef structuralModel
            obj.p     = reshape(statics{1}.str.p,6,constant{1}.str.Ns+1)';
            obj.frdof = constant{1}.str.frdof;
            obj.fxdof = constant{1}.str.fxdof;
+           obj.fxdof = constant{1}.str.fxdof;
+           % Save array
+           curdir = cd;
+           cd('ext/PROTEUS/results')
+           save('constant.mat','constant')
+           cd(curdir)
        end
        function obj = solve(obj,F)
            % Format 
            % (Nx, Ny, Nz, Mx, My, Mz)_per node
            obj.p = obj.Ks(obj.frdof,obj.frdof)\F(obj.frdof,1);
+       end
+       function obj = plot(obj,varargin)
+           load('constant');
+           if isempty(varargin)
+               plotBeamElements(constant{1})
+           else
+               delta = varargin{1};
+               figure()
+               ax1 = axes;
+               plotBeamElements(constant{1},0.3,0.03)
+               colormap(ax1,'gray');
+               freezeColors(ax1);
+               plotBeamElements(constant{1},delta,1,0.1)
+               colormap(ax1,'hot');
+           end
+       end
+       function obj = plotT3(obj)
+           figure()
+           hold on
+           grid on
+           plot(obj.grid(:,2),obj.p(:,3),'k');
+           xlabel('Span [m]')
+           ylabel('Vertical Displacement [m]')
+       end
+       function obj = plotR2(obj)
+           figure()
+           hold on
+           grid on
+           plot(obj.grid(:,2),rad2deg(obj.p(:,5)),'k')
+           xlabel('Span [m]')
+           ylabel('Torsional Deflection [deg]')
        end
    end
 end
