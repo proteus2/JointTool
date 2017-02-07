de5c5bd0358d6742b78e72fc3b48f384199d0793
sbind <simon.binder88@googlemail.com>
2017-02-07 08:03:58 +0100

    undo

--- frontend.m
+++ frontend.m
@@ -10,9 +10,12 @@ addpath(genpath('linStaticStrModule'))
 
 %% think about a way of putting states into an/another input file
 %                       U   aoa beta    Ma      rho
-
+ state = class_aero_state(100, 10*pi/180, 0,      0.0,    1.225);
 %% Setup Aero Model
 
+myAeroModel=AeroModel('crmWing4dAEDalus.xml',state);
+myAeroModel=myAeroModel.calculateForces();
+myAeroModel.plotCp();
 
 %% Setup Strutural Model
 % Class init.
